ce75aa8b501c27a7f898bbdf10e8fd0e76f3fa22
Yurii Novikov <104870932+Urichcool@users.noreply.github.com>
2023-02-06 21:30:55 +0100

    finish contacts.js

--- contacts.js
+++ contacts.js
@@ -2,7 +2,6 @@ const fs = require("fs").promises;
 const path = require("path");
 
 const contactsPath = path.resolve("db/contacts.json");
-console.log(contactsPath);
 
 function listContacts() {
   fs.readFile(contactsPath)
@@ -12,17 +11,45 @@ function listContacts() {
 
 function getContactById(contactId) {
   fs.readFile(contactsPath)
-    .then((data) => console.table(JSON.parse(data).find(contact => Number(contact.id) === contactId)))
+    .then((data) =>
+      console.table(
+        JSON.parse(data).find((contact) => Number(contact.id) === contactId)
+      )
+    )
     .catch((err) => console.error(err.message));
 }
 
 function removeContact(contactId) {
-  // ...твій код
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const contacts = JSON.parse(data);
+      const index = contacts.findIndex(
+        (contact) => Number(contact.id) === contactId
+      );
+      contacts.splice(index, 1);
+      return contacts;
+    })
+    .then((contacts) =>
+      fs.writeFile("db/contacts.json", JSON.stringify(contacts))
+    )
+    .catch((err) => console.error(err.message));
 }
 
 function addContact(name, email, phone) {
-
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const contacts = JSON.parse(data);
+      contacts.push({
+        id: String(contacts.length + 1),
+        name: name,
+        email: email,
+        phone: phone,
+      });
+      return contacts;
+    })
+    .then((contacts) =>
+      fs.writeFile("db/contacts.json", JSON.stringify(contacts))
+    )
+    .catch((err) => console.error(err.message));
 }
-
-
 
